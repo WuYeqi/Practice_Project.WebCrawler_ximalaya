6c5dd7609c7903a81b49b2f55ee510939c3aa145
wuyeqi <2812595904@qq.com>
2024-07-30 11:30:44 +0800

    重构代码，判断登陆状态自动登录并缓存cookies

--- Practice_Project.WebCrawler_ximalaya/ResourceDownloader_ximalaya.py
+++ Practice_Project.WebCrawler_ximalaya/ResourceDownloader_ximalaya.py
@@ -1,14 +1,12 @@
 import argparse
 import json
 import os
-import random
-import re
 import sys
 import time
 
 
 from bs4 import BeautifulSoup
-from selenium import webdriver
+import requests.cookies
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -21,17 +19,18 @@ from sources_rewritten.getSoundCryptLink import JSReverse
 from WebCrawlerCommonConf.WebdriverConf import WebdriverOptionsConf
 from WebCrawlerCommonConf.TransferConf import HeadersConf
 from FileCommonOperations.FileName import *
+from CrackCaptcha.PuzzleSlider import Slider
 
-
-import http.cookiejar as HC
+from selenium.webdriver.common.action_chains import ActionChains
+from requests.cookies import RequestsCookieJar
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from matplotlib import pyplot as PLT
+import cv2
 
 
 class WebCrawler_ximalaya:
 
     def __init__(self):
-        driver_conf = WebdriverOptionsConf()
-        options = driver_conf.get_options()
-        self.driver = webdriver.Chrome(options=options)
 
         # 获取当前脚本的完整路径
         PROJECT_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -43,6 +42,10 @@ class WebCrawler_ximalaya:
 
         ## 文件下载保存路径
         self.save_dir = yaml_data["basic"]["save_dir"]
+        self.account = {
+            "accountName": yaml_data["basic"]["account"]["accountName"],
+            "accountPWD": yaml_data["basic"]["account"]["accountPWD"],
+        }
 
         ## 预存的headers
         self.headers = yaml_data["transfer"]["headers"]
@@ -53,8 +56,13 @@ class WebCrawler_ximalaya:
             "\\", "/"
         )
 
+        # 临时文件夹
+        self.tmp_dir = os.path.join(PROJECT_BASE_DIR, "tmp_dir").replace("\\", "/")
+        if not self.tmp_dir:
+            os.makedirs(self.tmp_dir)
+
         # 相关接口
-        self.base_url = yaml_data["transfer"]["URL"]["base_album_url"]
+        self.album_url = yaml_data["transfer"]["URL"]["base_album_url"]
         self.api_tracks_list = yaml_data["transfer"]["URL"]["api_tracks_list"]
         self.api_tracks_info = yaml_data["transfer"]["URL"]["api_tracks_info"]
 
@@ -63,28 +71,51 @@ class WebCrawler_ximalaya:
         parser.add_argument("--id", type=int)
         self.args = parser.parse_args()
 
+        self.WDOC = WebdriverOptionsConf()
+        chromedriver_path = r"C:\Users\QiQi\AppData\Local\Programs\Python\Python312\Scripts\chromedriver.exe"
+        self.driver = self.WDOC.get_driver(chromedriver_path)
+
     def download_album(self):
         driver = self.driver
+
         album_id = str(self.args.id)
-        url = self.base_url + album_id
+        url = self.album_url + album_id
 
         driver.get(url)
-        # Webdriver添加cookies 自动登录
+        driver.maximize_window()
         with open(self.cookies_file, "r") as f:
             cookies = json.load(f)
             for cookie in cookies:
                 driver.add_cookie(cookie)
+
         driver.refresh()
 
-        # 为requests 获取cookies
+        # 悬停右上角头像，判断登陆状态
+        time.sleep(1)
+        actions = ActionChains(driver)
+        locator = (By.XPATH, '//*[@id="rootHeader"]/div/div[2]/div')
+        self.WDOC.waiting_until(locator)
+        element = driver.find_element(By.XPATH, '//*[@id="rootHeader"]/div/div[2]/div')
+        actions.move_to_element(element).perform()
+
+        # cookies已过期，账号未登录
+        try:
+            driver.find_element(By.XPATH, '//div[contains(text(),"个人页")]')
+        except NoSuchElementException as e:
+            self.auto_login()
+
+        # requests 从文件中获取cookies
         cookies_dict = {}
-        for item in driver.get_cookies():
-            cookies_dict[item["name"]] = item["value"]
+        with open(self.cookies_file, "r") as f:
+            cookies = json.load(f)
+            for item in cookies:
+                cookies_dict[item["name"]] = item["value"]
 
-        load_waiting = WebDriverWait(driver, 30)
+        # 开始下载
         XPATH_sound_list = "//div[@class='sound-list  H_g']"
-        load_waiting.until(EC.presence_of_element_located((By.XPATH, XPATH_sound_list)))
+        self.WDOC.waiting_until((By.XPATH, XPATH_sound_list))
 
+        session = requests.Session()
         # 专辑标题 albumTitle
         soup = BeautifulSoup(driver.page_source, features="lxml")
         driver.close()
@@ -101,9 +132,6 @@ class WebCrawler_ximalaya:
 
         is_download_continue = True
         pageNum = 1  # 当前页码
-
-        session = requests.Session()
-        requests.utils.add_dict_to_cookiejar(session.cookies, cookies_dict)
 
         while is_download_continue:
 
@@ -131,6 +159,7 @@ class WebCrawler_ximalaya:
                 track_title = legalized_file_name(track["title"])
 
                 # 反爬 - 修改请求头 headers中的 Referer 字段
+                self.headers["Host"] = "www.ximalaya.com"
                 self.headers["Referer"] = "https://www.ximalaya.com/sound/" + str(
                     track["trackId"]
                 )
@@ -158,10 +187,12 @@ class WebCrawler_ximalaya:
                     if not os.path.exists(save_dir):
                         os.mkdir(save_dir)
                 except FileNotFoundError as e:
-                    print("文件下载保存路径异常，请检查设置")
+                    print("文件下载保存路径异常，稍后请检查设置")
 
                 file_path = os.path.join(save_dir, file_save_name)
-                response = session.get(track_url, headers=self.headers)
+
+                # 迷惑，get加了headers反而会403Forbidden
+                response = session.get(track_url)
                 with open(file_path, "wb") as f:
                     print(
                         time.strftime("%H:%M:%S", time.localtime()),
@@ -176,6 +207,109 @@ class WebCrawler_ximalaya:
                     f.write(response.content)
 
             pageNum += 1
+
+    def auto_login(self):
+        driver = self.driver
+        driver.find_element(By.XPATH, '//div[contains(text(),"账号")]').click()
+
+        driver.find_element(By.XPATH, '//div[@class="login-tab-btn"]').click()
+
+        input_accountName = driver.find_element(By.XPATH, '//input[@id="accountName"]')
+        input_accountPWD = driver.find_element(By.XPATH, '//input[@id="accountPWD"]')
+        login_btn = driver.find_element(By.XPATH, '//button[@class="login-btn"]')
+
+        input_accountName.send_keys(self.account["accountName"])
+        input_accountPWD.send_keys(self.account["accountPWD"])
+        login_btn.click()
+
+        # 突破概率出现的拼图滑块验证码
+        time.sleep(1)
+        captcha_xpath = '//div[@class="mmv-panel WU_"]'
+        try:
+            while driver.find_element(By.XPATH, captcha_xpath):
+
+                locator = (By.XPATH, '//div[@class="__xmca-block"]')
+                self.WDOC.waiting_until(locator)
+                slide_block = driver.find_element(
+                    By.XPATH, '//div[@class="__xmca-block"]'
+                )
+
+                bg_img_xpath = '//img[@class="__xmca-img-main"]'
+                slide_img_xpath = '//img[@class="__xmca-img-bl"]'
+                # 获取验证码图片资源
+                bg_img, slide_img = self.get_captcha_img_resources(
+                    bg_img_xpath, slide_img_xpath
+                )
+
+                slider = Slider()
+                img_drag_distance = slider.get_drag_distance(bg_img, slide_img)
+
+                # 模拟鼠标拖动
+                calibrate_params = {
+                    # 误差校准
+                    "slide_rail_distance": 378.67,  # 滑轨总长
+                    "slide_btn_diameter": 38,  # 滑轨按钮直径
+                    "fixed_offset": 0.35294117647059053,  # 拼图固有偏差
+                }
+                actions = ActionChains(driver)
+                actions.click_and_hold(slide_block).perform()
+                actions.move_by_offset(
+                    slider.calibrate(img_drag_distance, calibrate_params), 0
+                ).release().perform()
+
+                os.remove(bg_img["path"])
+                os.remove(slide_img["path"])
+
+                time.sleep(1)
+        except NoSuchElementException as e:
+            pass
+
+        cookies = json.dumps(driver.get_cookies())
+        # 更新本地cookies json文件
+        with open(self.cookies_file, "w") as f:
+            f.write(cookies)
+
+    # 获取验证码图片资源
+    def get_captcha_img_resources(self, bg_img_xpath, slide_img_xpath):
+        driver = self.driver
+
+        pattern = "[^/]+(?!.*/)"
+        # 获取验证码背景大图
+        bg_img_src = driver.find_element(By.XPATH, bg_img_xpath).get_attribute("src")
+        bg_img_name = re.search(pattern, bg_img_src).group(0)
+        bg_img_name = self.tmp_dir + "/" + bg_img_name
+
+        bg_img = requests.get(bg_img_src)
+        with open(bg_img_name, "wb") as f:
+            f.write(bg_img.content)
+
+        # 获取验证码滑块小图
+        slide_img_src = driver.find_element(By.XPATH, slide_img_xpath).get_attribute(
+            "src"
+        )
+        slide_img_name = re.search(pattern, slide_img_src).group(0)
+        slide_img_name = self.tmp_dir + "/" + slide_img_name
+
+        slide_img = requests.get(slide_img_src)
+        with open(slide_img_name, "wb") as f:
+            f.write(slide_img.content)
+
+        bg_img = cv2.imread(bg_img_name)
+        slide_img = cv2.imread(slide_img_name, cv2.IMREAD_UNCHANGED)
+
+        bg_img = {
+            "cv2": bg_img,
+            "path": bg_img_name,
+            "initial_height": bg_img.shape[0],
+            "initial_width": bg_img.shape[1],
+        }
+        slide_img = {
+            "cv2": slide_img,
+            "path": slide_img_name,
+            "initial_height": slide_img.shape[0],
+            "initial_width": slide_img.shape[1],
+        }
+        return bg_img, slide_img
 
 
 WebCrawler_ximalaya().download_album()
